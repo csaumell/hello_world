9c42b733c4ef5cfab20f0e9bae04e15fdafbc974
csaumell <32459475+csaumell@users.noreply.github.com>
2017-10-04 19:41:55 +0200

    Add files via upload

--- hello_world.java
+++ hello_world.java
@@ -2,6 +2,6 @@ public class Hello{
    
     public static void main(String[] args){
 
-        System.out.println("Hello World: To forgive all is as inhuman as to forgive none 		(Seneca). Fortunately we have GIT");  
+        System.out.println("Bonjour Monde");  
     }
 }
